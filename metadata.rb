190f0d9750eb5bebc972f2e64e08201a025ab958
Seth Vargo <sethvargo@gmail.com>
2013-09-07 17:43:56 -0400

    Bump devodd release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures the 7-zip file archiver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 supports         "windows"
 
 depends          "windows", ">= 1.2.2"
