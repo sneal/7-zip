1eca9fcd116c9fb25f83790b26887918e52c873f
Jon Burgess <jkburges@gmail.com>
2015-12-24 15:07:50 +1100

    A few fixes
    - quote paths, filenames
    - bug fix with user specified path
    - always unzip (it's cheap)

--- resources/package.rb
+++ resources/package.rb
@@ -3,28 +3,26 @@ resource_name :seven_zip_package
 property :app, kind_of: String, name_property: true
 property :source, kind_of: String, default: nil
 property :owner, kind_of: String, default: nil
-property :destination, kind_of: String, default: ENV['SYSTEMDRIVE']
+property :destination, kind_of: String, default: nil
 property :checksum, kind_of: String, default: nil
 
 action :install do
   include_recipe 'seven-zip'
 
   seven_zip_file = ::File.join(Chef::Config[:file_cache_path], "#{app}.7z")
-  seven_zip_dest = ::File.join(new_resource.destination, app)
+  seven_zip_dest = new_resource.destination || ::File.join(ENV['SYSTEMDRIVE'], new_resource.app)
 
   directory seven_zip_dest do
     recursive true
-  end
-
-  execute 'unzip seven_zip_file' do
-    command "#{::File.join(node['7-zip']['home'], '7z')} e -y -o#{seven_zip_dest} #{seven_zip_file}"
-    action :nothing
   end
 
   remote_file seven_zip_file do
     source new_resource.source
     checksum new_resource.checksum if new_resource.checksum
     only_if { new_resource.source }
-    notifies :run, 'execute[unzip seven_zip_file]', :immediately
+  end
+
+  execute 'unzip seven_zip_file' do
+    command %Q^#{::File.join(node['7-zip']['home'], '7z')} e -y -o"#{seven_zip_dest}" "#{seven_zip_file}"^
   end
 end
